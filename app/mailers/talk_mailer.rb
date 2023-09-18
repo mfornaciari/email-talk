7cc4c3952439800c777f35d7ac925dbe92a02fec
Marco Fornaciari <marco.fornaciari@rebase.com.br>
2023-09-18 14:15:27 -0300

    add Outlook e-mail and banana image

--- app/mailers/talk_mailer.rb
+++ app/mailers/talk_mailer.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 class TalkMailer < ApplicationMailer
-  default from: 'marco.fornaciari@rebase.com.br'
+  before_action :set_attachment
 
-  def test_email
-    mail to: 'marco.fornaciari@rebase.com.br', subject: 'E-mail da banana'
+  default from: 'marco.fornaciari@rebase.com.br',
+          subject: 'E-mail da banana'
+
+  def gmail_mail
+    mail to: 'marco.fornaciari@rebase.com.br'
+  end
+
+  def outlook_mail
+    mail to: 'marco.fornaciari.rebase@outlook.com'
+  end
+
+  private
+
+  def set_attachment
+    attachments.inline['banana.png'] = File.read(Rails.public_path.join('images', 'banana.png'))
   end
 end
--- test/mailers/previews/talk_mailer_preview.rb
+++ test/mailers/previews/talk_mailer_preview.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class TalkMailerPreview < ActionMailer::Preview
-  def test_email
-    TalkMailer.test_email
+  def gmail_mail
+    TalkMailer.gmail_mail
+  end
+
+  def outlook_mail
+    TalkMailer.outlook_mail
   end
 end
